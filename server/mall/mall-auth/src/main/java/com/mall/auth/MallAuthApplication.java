429ef31eb9e72b12372f28cd12331618d945ee80
ZhangKun <1016297824@qq.com>
2026-05-25 20:43:35 +0800

    bean重复问题修正

--- server/mall/mall-auth/src/main/java/com/mall/auth/MallAuthApplication.java
+++ server/mall/mall-auth/src/main/java/com/mall/auth/MallAuthApplication.java
@@ -10,8 +10,8 @@ import org.mybatis.spring.annotation.MapperScan;
  * mall-auth 认证服务
  * 端口：9210
  */
-@EnableFeignClients(basePackages = {"com.ruoyi", "com.mall.api"})
-@MapperScan("com.mall.auth.mapper")
+@EnableFeignClients(basePackages = "com.mall.api")
+//@MapperScan("com.mall.auth.mapper")
 @EnableDiscoveryClient
 @SpringBootApplication(scanBasePackages = {"com.mall.auth", "com.mall.common"})
 public class MallAuthApplication {
